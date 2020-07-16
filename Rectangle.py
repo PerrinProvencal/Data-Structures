ad02c6fd9f4ac67ae68b569eb0a9998c96d229b5
PerrinProvencal <Niisackitey13@gmail.com>
2020-07-16 23:00:12 +0000

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -1,23 +1,37 @@
-class point:  
+class point: 
+    
     def __init__(self, pt):
-        self.Xcord = Xcord
-        self.Ycord = Ycord
+        Xcord = 0
+        Ycord = 0 
         self.pt = (Xcord, Ycord)
    
-   
- 
 
 
 class Rectangle(point):
-    a = 0
-    b = 0
-    c = 0
-    d = 0
-    def __init__ (self, a,b,c,d,bottomLeftCorner, topRightCorner):
+    a=0
+    b=0
+    c=0
+    d=0
+
+
+    def __init__ (self,a,b,c,d):
+        bottomLeftCorner = ()
+        topRightCorner = ()
         x = point(bottomLeftCorner)
         y = point(topRightCorner) 
         x.pt =(a,b)
         y.pt = (c,d)
 
-    def area(x,y)
-  
+    def area(self,x,y):
+        ar = (y.pt[0] - x.pt[0]) * (y.pt[1]-x.pt[1])
+        return ar
+
+    def perimeter(self,x,y):
+        per = 2*(y.pt[0] - x.pt[0]) + (y.pt[1]-x.pt[1])
+        return per
+
+r1 =  Rectangle(1,2,4,5)
+x = point()
+y=point()
+print(r1.area(x,y))
+print(r1.perimeter(x,y))
