288222774d51daca6dba71e933ad4c9d678df47d
PerrinProvencal <Niisackitey13@gmail.com>
2020-07-24 23:43:56 +0000

    Update Deque.py

--- Deque.py
+++ Deque.py
@@ -50,46 +50,49 @@ for i in range(Deq.MaxArraySize//2):
     fig = random.randint(1,100)
     Deq.add_to_front(fig)
 
-pro = random.uniform(0,1)
-if pro > 0 and pro <= 0.1:
-    a = random.randint(0,100)
-    Deq.add_to_front(a)
-
-elif pro > 0.1 and pro <= 0.3:
-    Deq.remove_front()
-
-elif pro > 0.3 and pro < 0.4:
-    a = random.randint(1,100)
-    Deq.add_to_back(a)
-
-else:
-    Deq.remove_back()
-
 print("First Simulation")
+for j in range(50):
+    pro = random.uniform(0,1)
+    if pro > 0 and pro <= 0.1:
+        a = random.randint(0,100)
+        Deq.add_to_front(a)
 
-print(Deq.array)
-print(Deq.size())
-print(Deq.isEmpty())
-print(Deq.isFull())
+    elif pro > 0.1 and pro <= 0.3:
+        Deq.remove_front()
 
-pro2 = random.uniform(0,1)
-if pro2 > 0 and pro2 <= 0.1:
-    b = random.randint(0,100)
-    Deq.add_to_front(b)
+    elif pro > 0.3 and pro < 0.4:
+        a = random.randint(1,100)
+        Deq.add_to_back(a)
 
-elif pro2 > 0.1 and pro2 <= 0.7:
-    Deq.remove_front()
+    else:
+        Deq.remove_back()
 
-elif pro2 > 0.7 and pro2 <= 0.8:
-    b = random.randint(1,100)
-    Deq.add_to_back(b)
-
-else:
-    Deq.remove_back()
+    print(Deq.array)
+    print(Deq.size())
+    print(Deq.isEmpty())
+    print(Deq.isFull())
 
 print("")
 print("Second Simulation")
-print(Deq.array)             
-print(Deq.size())
-print(Deq.isEmpty())
-print(Deq.isFull())
+for k in range(50):
+        
+    pro2 = random.uniform(0,1)
+    if pro2 > 0 and pro2 <= 0.1:
+        b = random.randint(0,100)
+        Deq.add_to_front(b)
+
+    elif pro2 > 0.1 and pro2 <= 0.7:
+        Deq.remove_front()
+
+    elif pro2 > 0.7 and pro2 <= 0.8:
+        b = random.randint(1,100)
+        Deq.add_to_back(b)
+
+    else:
+        Deq.remove_back()
+
+   
+    print(Deq.array)             
+    print(Deq.size())
+    print(Deq.isEmpty())
+    print(Deq.isFull())
